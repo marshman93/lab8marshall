c8c22dc38ddc787c13bc281e0dd5276a128ddeba
marshman93 <56427571+marshman93@users.noreply.github.com>
2020-05-01 11:21:47 -0700

    Day #1 Progress
    Almost finished with the dictionary for all the players that will be able to be drafted in the game.

--- BasicNBAGMgame.py
+++ BasicNBAGMgame.py
@@ -1,1 +1,7 @@
-# First commit just as a test
+playerDict = {
+    'LeBron James': 97, 'Giannis Antetokounmpo': 97, 'Kawhi Leonard': 96, 'Kevin Durant': 95, 'Anthony Davis': 95,
+    'James Harden': 94, 'Stephen Curry': 94, 'Damian Lillard': 93, 'Luka Doncic': 92, 'Nikola Jokic': 92,
+    'Kyrie Irving': 91, 'Joel Embiid': 91, 'Karl-Anthony Towns': 91, 'Trae Young': 90, 'Paul George': 90,
+    'Russell Westbrook': 89, 'Pascal Siakam': 88, 'Klay Thompson': 88, 'Ben Simmons': 87, 'Bradley Beal': 87,
+    'Jayson Tatum': 86, 'Jimmy Butler': 86, 'Donovan Mitchell': 84, 'Chris Paul': 84, 'Rudy Gobert': 84,
+}
