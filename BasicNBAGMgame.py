c6100ec935a88b8b858dc9f8412a055e475863a8
marshman93 <56427571+marshman93@users.noreply.github.com>
2020-05-19 02:21:41 -0700

    Work Session #2
    Added more players

--- BasicNBAGMgame.py
+++ BasicNBAGMgame.py
@@ -1,7 +1,15 @@
 playerDict = {
-    'LeBron James': 97, 'Giannis Antetokounmpo': 97, 'Kawhi Leonard': 96, 'Kevin Durant': 95, 'Anthony Davis': 95,
-    'James Harden': 94, 'Stephen Curry': 94, 'Damian Lillard': 93, 'Luka Doncic': 92, 'Nikola Jokic': 92,
-    'Kyrie Irving': 91, 'Joel Embiid': 91, 'Karl-Anthony Towns': 91, 'Trae Young': 90, 'Paul George': 90,
-    'Russell Westbrook': 89, 'Pascal Siakam': 88, 'Klay Thompson': 88, 'Ben Simmons': 87, 'Bradley Beal': 87,
-    'Jayson Tatum': 86, 'Jimmy Butler': 86, 'Donovan Mitchell': 84, 'Chris Paul': 84, 'Rudy Gobert': 84,
+    'LeBron James': 97, 'Giannis Antetokounmpo': 97, 'James Harden': 97, 'Kevin Durant': 96, 'Kawhi Leonard': 96,
+    'Luka Doncic': 96, 'Anthony Davis': 96, 'Stephen Curry': 95, 'Damian Lillard': 94, 'Nikola Jokic': 91, 'Joel Embiid': 91
+    'Kyrie Irving': 91, 'Trae Young': 90, 'Karl-Anthony Towns': 90, 'Paul George': 90, 'Russell Westbrook': 89, 'Pascal Siakam': 89,
+    'Bradley Beal': 89, 'Klay Thompson': 89, 'Ben Simmons': 88, 'Jayson Tatum': 88, 'Jimmy Butler': 88, 'Donovan Mitchell': 87,
+    'Chris Paul': 87, 'Rudy Gobert': 87, 'Kemba Walker': 87, 'Khris Middleton': 87, 'Zion Williamson': 86, 'Kristaps Porzingis': 86,
+    'Devin Booker': 86, 'Brandon Ingram': 86, 'DeMar DeRozan': 86, 'John Wall': 86, 'Bam Adebayo': 86, 'John Collins': 86,
+    'Clint Capela': 86, 'DeAndre Ayton': 85, 'Andre Drummond': 85, 'DeAaron Fox': 85, 'C.J. McCollum': 85, 'Zach LaVine': 85,
+    'Nikola Vucevic': 85, 'Kyle Lowry': 85, 'Tobias Harris': 85, 'Jaylen Brown': 85, 'Montrezl Harrell': 85, 'Domantas Sabonis': 85,
+    'Ja Morant': 84, 'Shai Gilgeous-Alexander': 84, 'LaMarcus Aldridge': 84, 'DAngelo Russell': 84, 'Jamal Murray': 84,
+    'Derrick Rose': 84, 'Steven Adams': 84, 'Danilo Gallinari': 84, 'Mitchell Robinson': 83, 'Malcolm Brogdon': 83,
+    'Victor Oladipo': 83, 'Jrue Holiday': 83, 'Eric Bledsoe': 83
 }
+
+# This is the full dictionary of 60 players, to be spread out amongst 6 teams. There will be 10 teams each.
